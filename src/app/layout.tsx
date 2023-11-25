4f262a8a7bcc3e6f4b97f609087ce8e39834013b
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2023-11-26 01:05:14 +0700

    rafactor: service provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
 import "@/styles/globals.css";
+
 import Sidebar from "@/components/Sidebar";
-import SupabaseProvider from "../../packages/services/SupabaseProvider";
-import UserProvider from "../../packages/services/UserProvider";
+
+import SupabaseProvider from "@/services/SupabaseProvider";
+import UserProvider from "@/services/UserProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
