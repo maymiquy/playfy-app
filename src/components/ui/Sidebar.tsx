4d4b4b5e80807d6e8b0929d720fa2e82b97a9d7e
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2023-11-30 22:58:54 +0700

    feat: create auth custom hook

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -6,9 +6,9 @@ import { useMemo } from "react";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 
-import Box from "./Box";
-import SidebarItem from "./SidebarItem";
-import Library from "./Library";
+import Box from "../Box";
+import SidebarItem from "../SidebarItem";
+import Library from "../Library";
 
 interface SidebarProps {
  children: React.ReactNode;
